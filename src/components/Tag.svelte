3001de63c3ca50477d24fb783c23d868697df562
jtornert <joakim.tornert@outlook.com>
2022-02-03 20:33:25 +0100

    added filters

---
+++ src/components/Tag.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { Label } from "@smui/common";
+  import { createEventDispatcher } from "svelte";
+  export let selected = false;
+  let dispatch = createEventDispatcher();
+
+  function clickHandler() {
+    selected = !selected;
+    selected ? dispatch("enable") : dispatch("disable");
+  }
+</script>
+
+<span class:selected on:click={clickHandler}><slot /></span>
+
+<style>
+  span {
+    font-family: sans-serif;
+    padding: 0.2em 0.75em;
+    border-radius: 9999px;
+    color: rgb(98, 0, 238);
+    outline: 2px solid rgb(98, 0, 238);
+    outline-offset: -2px;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .selected {
+    background-color: rgb(98, 0, 238);
+    color: whitesmoke;
+  }
+</style>
--- src/views/Wardrobe.svelte
+++ src/views/Wardrobe.svelte
@@ -2,63 +2,53 @@
   import Fab, { Icon } from "@smui/fab";
   import Dialog from "@smui/dialog";
   import Button, { Label } from "@smui/button";
+  import Tag from "../components/Tag.svelte";
 
   const images = [
-    "./img/bottom-1.png",
-    "./img/bottom-2.png",
-    "./img/bottom-3.png",
-    "./img/shoes-1.png",
-    "./img/top-1.png",
-    "./img/top-2.png",
-    "./img/top-3.png",
-    "./img/top-4.png",
-    "./img/top-5.png",
-    "./img/top-6.png",
-    "./img/top-7.png",
-    "./img/top-8.png",
-    "./img/top-9.png",
-    "./img/bottom-1.png",
-    "./img/bottom-2.png",
-    "./img/bottom-3.png",
-    "./img/shoes-1.png",
-    "./img/top-1.png",
-    "./img/top-2.png",
-    "./img/top-3.png",
-    "./img/top-4.png",
-    "./img/top-5.png",
-    "./img/top-6.png",
-    "./img/top-7.png",
-    "./img/top-8.png",
-    "./img/top-9.png",
-    "./img/bottom-1.png",
-    "./img/bottom-2.png",
-    "./img/bottom-3.png",
-    "./img/shoes-1.png",
-    "./img/top-1.png",
-    "./img/top-2.png",
-    "./img/top-3.png",
-    "./img/top-4.png",
-    "./img/top-5.png",
-    "./img/top-6.png",
-    "./img/top-7.png",
-    "./img/top-8.png",
-    "./img/top-9.png",
-    "./img/bottom-1.png",
-    "./img/bottom-2.png",
-    "./img/bottom-3.png",
-    "./img/shoes-1.png",
-    "./img/top-1.png",
-    "./img/top-2.png",
-    "./img/top-3.png",
-    "./img/top-4.png",
-    "./img/top-5.png",
-    "./img/top-6.png",
-    "./img/top-7.png",
-    "./img/top-8.png",
-    "./img/top-9.png",
+    { image: "./img/bottom-1.png", tags: ["chill", "casual"] },
+    { image: "./img/bottom-2.png", tags: ["professional", "party"] },
+    { image: "./img/bottom-3.png", tags: ["casual", "trendy"] },
+    { image: "./img/shoes-1.png", tags: ["trendy", "party"] },
+    { image: "./img/top-1.png", tags: ["casual"] },
+    { image: "./img/top-2.png", tags: ["professional", "trendy"] },
+    { image: "./img/top-3.png", tags: ["casual", "professional"] },
+    { image: "./img/top-4.png", tags: ["professional"] },
+    { image: "./img/top-5.png", tags: ["chill", "trendy"] },
+    { image: "./img/top-6.png", tags: ["chill"] },
+    { image: "./img/top-7.png", tags: ["casual", "professional", "party"] },
+    { image: "./img/top-8.png", tags: ["casual", "party"] },
+    { image: "./img/top-9.png", tags: ["chill", "casual", "sporty"] },
   ];
 
   let open = false;
+  let filters = [];
+  let displayedClothes = [...images];
+
+  function renderList() {
+    if (filters.length === 0) {
+      displayedClothes = images;
+      return;
+    }
+
+    displayedClothes = [];
+    for (const filter of filters) {
+      for (const image of images) {
+        if (image.tags.includes(filter) && !displayedClothes.includes(image)) {
+          displayedClothes.push(image);
+        }
+      }
+    }
+  }
+
+  function addFilter(tag) {
+    filters.push(tag);
+    renderList();
+  }
+
+  function removeFilter(tag) {
+    filters = filters.filter((value) => value !== tag);
+    renderList();
+  }
 </script>
 
 <main>
@@ -78,9 +68,35 @@
     </Fab>
   </span>
 
+  <div class="tag-list">
+    <Tag
+      on:enable={() => addFilter("casual")}
+      on:disable={() => removeFilter("casual")}>Casual</Tag
+    >
+    <Tag
+      on:enable={() => addFilter("chill")}
+      on:disable={() => removeFilter("chill")}>Chill</Tag
+    >
+    <Tag
+      on:enable={() => addFilter("sporty")}
+      on:disable={() => removeFilter("sporty")}>Sporty</Tag
+    >
+    <Tag
+      on:enable={() => addFilter("trendy")}
+      on:disable={() => removeFilter("trendy")}>Trendy</Tag
+    >
+    <Tag
+      on:enable={() => addFilter("party")}
+      on:disable={() => removeFilter("party")}>Party</Tag
+    >
+    <Tag
+      on:enable={() => addFilter("professional")}
+      on:disable={() => removeFilter("professional")}>Professional</Tag
+    >
+  </div>
   <ul class="image-list">
-    {#each images as image}
-      <img src={image} alt="clothes" />
+    {#each displayedClothes as image}
+      <img src={image.image} alt="clothes" />
     {/each}
   </ul>
 </main>
@@ -96,6 +112,15 @@
     position: fixed;
     bottom: 5em;
     right: 1em;
+  }
+
+  .tag-list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.2em;
+    padding-bottom: 1em;
+    margin-bottom: 1em;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
   }
 
   .image-list {
